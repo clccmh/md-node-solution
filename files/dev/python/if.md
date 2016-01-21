3517e0a524b995bbff1c04fb3b8513f900690f5c
Carter Hay <carter@carterhay.net>
2016-01-21 12:54:35 -0600

    implemented recursion in update

---
+++ files/dev/python/if.md
@@ -0,0 +1,7 @@
+//If
+//How to use if in python
+//if, python, basics
+
+#If
+An if statement is used for selection of two things:
+    if i == 0:
--- quicktest.py
+++ quicktest.py
@@ -1,8 +1,4 @@
-import argparse
-import glob
-import os
-import webbrowser
-import markdown
+import argparse, fnmatch, os, webbrowser, markdown
 
 parser = argparse.ArgumentParser(description='Actions for notes')
 parser.add_argument('-u', action='store_true', help='Update the file db')
@@ -39,18 +35,20 @@ if args.u:
     out = open("files.conf", "w")
     out.seek(0)
     out.truncate()
-    for filename in glob.glob('*.md'):
-        print(filename)
-        title = ""
-        description = ""
-        tags = ""
-        f = open(filename, "r")
-        for line in f:
-            if line[0] == "/" and line[1] == "/":
-                fline = line.strip("//").split(" ")
-                if fline[0] == "Title:": title = fline[1].strip("\n")
-                if fline[0] == "Description:": description = ' '.join(fline[1:]).strip("\n")
-                if fline[0] == "Tags:": tags = ''.join(fline[1:]).strip('\n')
-        out.write(os.path.abspath(filename) + ',' + title + ',' + description + ',' + tags + '\n')
+    for root, dirnames, filenames in os.walk('files'):
+        for filename in fnmatch.filter(filenames, '*.md'):
+            filename = os.path.join(root, filename)
+            print(filename)
+            title = ""
+            description = ""
+            tags = ""
+            f = open(filename, "r")
+            for line in f:
+                if line[0] == "/" and line[1] == "/":
+                    fline = line.strip("//").split(" ")
+                    if fline[0] == "Title:": title = fline[1].strip("\n")
+                    if fline[0] == "Description:": description = ' '.join(fline[1:]).strip("\n")
+                    if fline[0] == "Tags:": tags = ''.join(fline[1:]).strip('\n')
+            out.write(os.path.abspath(filename) + ',' + title + ',' + description + ',' + tags + '\n')
 
 
