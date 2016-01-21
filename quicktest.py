728eaa5145369a17c7c2f6b5d2b252fe07aaebb7
Carter Hay <chay@uno.edu>
2016-01-21 11:58:17 -0600

    implemented searching of tags

--- quicktest.py
+++ quicktest.py
@@ -1,10 +1,38 @@
 import argparse
 import glob
 import os
+import webbrowser
+import markdown
 
 parser = argparse.ArgumentParser(description='Actions for notes')
 parser.add_argument('-u', action='store_true', help='Update the file db')
+parser.add_argument('-t', action='store', help='Search through the tag, or the description (faster)')
 args = parser.parse_args()
+
+def handleSelection(options):
+    for x in range(0, len(options)):
+        print("%d) %s" % (x, options[0]))
+    choice = input("Select the number you want to view, or q to quit: ")
+    if choice != "q":
+        choice = int(choice)
+        with open(options[choice], "r") as f:
+            with open("temp.html", "w") as o:
+                o.seek(0)
+                o.truncate()
+                text = ""
+                for line in f:
+                    if line[0] != "/":
+                        text += line
+                o.write(markdown.markdown(text))
+                webbrowser.open_new(os.path.abspath("temp.html"))
+
+if args.t:
+    options = []
+    with open("files.conf", "r") as f:
+        for line in f:
+            if args.t in line:
+                options.append(line.split(",")[0])
+    handleSelection(options)
 
 if args.u:
     #Open our file, and remove everything from it
@@ -24,3 +52,5 @@ if args.u:
                 if fline[0] == "Description:": description = ' '.join(fline[1:]).strip("\n")
                 if fline[0] == "Tags:": tags = ''.join(fline[1:]).strip('\n')
         out.write(os.path.abspath(filename) + ',' + title + ',' + description + ',' + tags + '\n')
+
+
